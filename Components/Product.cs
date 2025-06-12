2ad10c5594162d447a62d19277f3b27e6ca3296d
tkfx-code <tkfx.se@gmail.com>
2025-06-12 11:35:02 +0200

    Added ProductList components

---
+++ Components/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace V24_ToDoList.Components
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product()
+        {
+            
+        }
+        public Product(string name, int quantity, double price)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
